e12a010d590dbd3b40ad75ee27a18aa02292cf98
Ltrejosb <deftrejos@gmail.com>
2023-01-17 00:03:33 -0300

    Storage + DOM + Eventos

--- js/main.js
+++ js/main.js
@@ -1,87 +1,83 @@
-// Array para la clasificacion de hombres
-var clasificacionHombre = [
-  {
-    rango: 'Bajo peso',
-    recomendacion: 'Considera aumentar tu ingesta de proteínas y calorías para ganar peso.'
-  },
-  {
-    rango: 'Peso normal',
-    recomendacion: '¡Mantén el buen trabajo!'
-  },
-  {
-    rango: 'Sobrepeso',
-    recomendacion: 'Considera reducir tu ingesta de calorías y aumentar tu actividad física para perder peso.'
-  },
-  {
-    rango: 'Obeso',
-    recomendacion: 'Es importante consultar a un profesional de la salud para desarrollar un plan de pérdida de peso.'
-  }
-];
+// Verificar si hay datos en el Local Storage
 
-// Array para clasificacion de mujeres
-var clasificacionMujer = [
-  {
-    rango: 'Bajo peso',
-    recomendacion: 'Considera aumentar tu ingesta de proteínas y calorías para ganar peso.'
-  },
-  {
-    rango: 'Peso normal',
-    recomendacion: '¡Mantén el buen trabajo!'
-  },
-  {
-    rango: 'Sobrepeso',
-    recomendacion: 'Considera reducir tu ingesta de calorías y aumentar tu actividad física para perder peso.'
-  },
-  {
-    rango: 'Obeso',
-    recomendacion: 'Es importante consultar a un profesional de la salud para desarrollar un plan de pérdida de peso.'
-  }
-];
-  //Se une js a el boton "calcular" 
-document.querySelector('form').addEventListener('submit', function(event) {
-  event.preventDefault();
+let data = JSON.parse(localStorage.getItem("imcData")) || [];
 
-  // Obtenemos los valores de peso, altura y genero
-  var peso = document.querySelector('#pesoInput').value;
-  var altura = document.querySelector('#alturaInput').value;
-  var genero = document.querySelector('#selectorGenero').value;
+// Cargar los datos guardados en el Local Storage a la tabla
 
-  // Validamos la información
-  if (peso <= 0 || altura <= 0) {
-    document.querySelector('#result').innerHTML = 'Datos de entrada inválidos';
-    return;
-  }
-
-  // Calculamos el IMC
-  var imc = peso / (altura / 100) ** 2;
-
-  // Inicializamos el mensaje de resultado
-var resultado = 'Tu IMC es ' + imc.toFixed(2) + '. ';
-
-// Añadimos el mensaje segun el genero
-if (genero === 'hombre') {
-if (imc < 20) {
-resultado += clasificacionHombre[0].rango + '. ' + clasificacionHombre[0].recomendacion;
-} else if (imc >= 20 && imc < 25) {
-resultado += clasificacionHombre[1].rango + '. ' + clasificacionHombre[1].recomendacion;
-} else if (imc >= 25 && imc < 30) {
-resultado += clasificacionHombre[2].rango + '. ' + clasificacionHombre[2].recomendacion;
-} else {
-resultado += clasificacionHombre[3].rango + '. ' + clasificacionHombre[3].recomendacion;
-}
-} else {
-if (imc < 19) {
-resultado += clasificacionMujer[0].rango + '. ' + clasificacionMujer[0].recomendacion;
-} else if (imc >= 19 && imc < 24) {
-resultado += clasificacionMujer[1].rango + '. ' + clasificacionMujer[1].recomendacion;
-} else if (imc >= 24 && imc < 29) {
-resultado += clasificacionMujer[2].rango + '. ' + clasificacionMujer[2].recomendacion;
-} else {
-resultado += clasificacionMujer[3].rango + '. ' + clasificacionMujer[3].recomendacion;
-}
-}
-
-// Mostramos el resultado
-document.querySelector('#resultado').innerHTML = resultado;
+data.forEach(person => {
+    const table = document.getElementById("imcTable").getElementsByTagName('tbody')[0];
+    const row = table.insertRow();
+    Object.values(person).forEach(cellValue => {
+        const cell = row.insertCell();
+        cell.innerHTML = cellValue;
+    });
 });
+
+// Agregar evento al formulario para calcular el IMC al hacer submit
+
+document.getElementById("imcForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    
+    // Obtener los datos del formulario
+
+    let person = {
+        name: document.getElementById("name").value,
+        weight: document.getElementById("weight").value,
+        height: document.getElementById("height").value,
+        gender: document.getElementById("gender").value
+    };
+    
+    // Calcular el IMC
+
+    person.imc = (person.weight / (person.height / 100) ** 2).toFixed(2);
+    
+    // Asignar el consejo según el género y el IMC
+
+    if (person.gender === "hombre") {
+    if (person.imc < 20) person.advice = 'Estas debajo del peso recomendado, debes aumentar tu ingesta calórica y hacer ejercicio.';
+    else if (person.imc >= 20 && person.imc <= 25) person.advice = 'Estas en el peso recomendado, mantén tu dieta y hábitos de ejercicio.';
+    else person.advice = 'Estas por encima del peso recomendado, debes reducir tu ingesta calórica y hacer ejercicio.'
+    } else {
+    if (person.imc < 19) person.advice = 'Estas debajo del peso recomendado, debes aumentar tu ingesta calórica y hacer ejercicio.';
+    else if (person.imc >= 19 && person.imc <= 24) person.advice = 'Estas en el peso recomendado, mantén tu dieta y hábitos de ejercicio.';
+    else person.advice = 'Estas por encima del peso recomendado, debes reducir tu ingesta calórica y hacer ejercicio.'
+    }
+    
+    // Agregar la persona al arreglo de datos
+
+    data.push(person);
+    
+    // Guardar el arreglo de datos en el LocalStorage
+
+    localStorage.setItem("imcData", JSON.stringify(data));
+    
+    // Agregar la persona a la tabla
+
+    const table = document.getElementById("imcTable").getElementsByTagName('tbody')[0];
+    const row = table.insertRow();
+    Object.values(person).forEach(cellValue => {
+        const cell = row.insertCell();
+        cell.innerHTML = cellValue;
+        });
+    });
+
+    // Agregar evento al boton limpiar tabla
+
+document.getElementById("clear").addEventListener("click", function() {
+    
+    // Eliminar el contenido de la tabla
+
+    document.getElementById("imcTable").getElementsByTagName('tbody')[0].innerHTML = "";
+    
+    // Eliminar los datos guardados en el Local Storage
+
+    localStorage.removeItem("imcData");
+    
+    // Reiniciar el arreglo de datos
+    
+    data = [];
+});
+
+
+
 
